dd83953bd060e98cee7892ad50533445229ec2c6
Joseph Parker <jparker010190@gmail.com>
2018-05-06 02:08:17 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    eve = self.artists
+    eve = self.artists.name
     eve.to_s
   end
 end
